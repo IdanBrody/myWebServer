a1c0997281a2ed716a6afbf3505e3f2be7fb99f0
bensocho <bensocho10@gmail.com>
2023-06-22 17:31:14 +0300

    modified the handle_login with the hashed passwd and added jwt token

--- Database/Users/Login_User.py
+++ Database/Users/Login_User.py
@@ -5,21 +5,19 @@ from passlib.hash import sha256_crypt
 def login_user(username, password):
     # Get a connection to the database
     connection = connect_to_database()
-
     # Create a cursor object to execute SQL queries
     cursor = connection.cursor()
     # SQL query to check if the user exists
-    query = "SELECT * FROM users WHERE username = %s"
+    query = "SELECT password FROM users WHERE username = %s".format(username)
     # Execute the query and fetch the result
-    cursor.execute(query, username)
+    cursor.execute(query)
     result = cursor.fetchone()
-
     # Close the cursor and connection
     cursor.close()
     connection.close()
 
     # Check if the result is not None (user exists) and password is correct
-    if result is not None and sha256_crypt.verify(password, result["password"]):
+    if result is not None and sha256_crypt.verify(password, result):
         return True
     else:
         return False
--- Server/Main.py
+++ Server/Main.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template, request
 from Server.Login_SignUp import handle_sign_up, handle_login
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+import os
 
-
-
+secret_key = os.urandom(32)
 app = Flask(__name__)
+app.config["JWT_SECRET_KEY"] = secret_key
+jwt = JWTManager(app)
 app.secret_key = "idan2001"
 
 # Route for Home
 @app.route("/index")
 @app.route('/')
 def index():
+    user_name = get_jwt_identity() or "Guest"
     # Render the index.html template
-    return render_template('index.html')
+    return render_template('index.html', user_name=user_name)
 
 
 # Route for login
@@ -40,11 +44,15 @@ def dashboard():
 # Route for catalog
 @app.route('/catalog', methods=['GET'])
 def catalog():
-    return render_template('catalog.html')
+    user_name = get_jwt_identity() or "Guest"
+    return render_template('catalog.html', user_name=user_name)
+
 
 @app.route('/contact', methods=['GET'])
 def contact():
-    return render_template('contact.html')
+    user_name = get_jwt_identity() or "Guest"
+    return render_template('contact.html', user_name=user_name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
