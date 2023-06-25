87a3fbee30133e7967b1b21f3d36951670405229
idanbrody <idanbrody8@gmail.com>
2023-06-25 16:00:13 +0300

    New filter functionality is now visible to users through HTML code changes.

--- Server/Catalog.py
+++ Server/Catalog.py
@@ -3,17 +3,15 @@ from Database.MySQL_Connection import connect_to_database
 from flask import render_template
 
 
-def handle_catalog(user_name, category_filter=None, vendor_filter=None, gender_filter=None):
-    conditions = []
-    if category_filter is not None:
-        conditions.append("category={}".format(category_filter))
-    if vendor_filter is not None:
-        conditions.append("vendor={}".format(vendor_filter))
-    if gender_filter is not None:
-        conditions.append("gender={}".format(gender_filter))
-    query = "SELECT product_link FROM products"
-    if conditions:
-        query += " WHERE" + " AND".join(conditions)
+def handle_catalog(user_name, category_filter, vendor_filter, gender_filter):
+    query = "SELECT product_link FROM products WHERE 1=1"
+    if category_filter:
+        query += " AND category = '{}'".format(category_filter)
+    if vendor_filter:
+        query += " AND vendor = '{}'".format(vendor_filter)
+    if gender_filter:
+        query += " AND gender = '{}'".format(gender_filter)
+    print(query)
     connection = connect_to_database()
     cursor = connection.cursor()
     cursor.execute(query)
